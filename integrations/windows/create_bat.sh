fc488acc4b8ee0c9ead3a19f0338bddbe4253c82
Tomer Levy <tmrlvi@gmail.com>
2022-04-19 10:50:44 +0300

    Adding default bat file to windows

---
+++ integrations/windows/create_bat.sh
@@ -0,0 +1,3 @@
+echo :start > ${1}/mine.bat
+echo ${1}.exe -a kaspa:qz4jdyu04hv4hpyy00pl6trzw4gllnhnwy62xattejv2vaj5r0p5quvns058f -s n.seeder1.kaspad.net >> ${1}/mine.bat
+echo goto start >> ${1}/mine.bat
